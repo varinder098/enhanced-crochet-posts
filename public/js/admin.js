613b0d7a73c19fe780677f08c7a74e392f26bedc
harminderkaur9808 <harminderkaur9808@gmail.com>
2021-09-08 21:29:55 +0530

    Update admin.js

--- public/js/admin.js
+++ public/js/admin.js
@@ -7,39 +7,39 @@ $(document).ready(function() {
      
     
     if ($(".page_id").text() == "add") {
-        var x = 1;
-        var y = 1; 
+        var rowIdy = 1; 
         var rowIdx =1;
     } else {
         var rowIdx = $('.count').text();
-        var x = $('.count').text();
-        var y = $('.vcount').text();
+        var rowIdy = $('.vcount').text();
 
-        if(y==0) {
-            y=1;
+        if(rowIdy==0)
+        {
+            rowIdy=1;
         }
     }
 
     $(add_button).click(function(e) {
         e.preventDefault();
         x++;
-        $(append).append(`<tr id="R${++rowIdx}"><td><div class="ml-2 mb-2 d-flex"><span class="strong"> </span><input type="text" class="form-control" name="us[]"></div><p style=" margin-top: -29px;"> ${rowIdx}. </p></td><td><div class="mb-2 d-flex"><input type="text" class="form-control mr-2" name="uk[]"><a href="javascript:void(0)" class="btn btn-sm btn-danger delete">&times;</a></div></td></tr>`); //add input box
+        $(append).append(`<tr id="R${++rowIdx}"><td><div class="ml-2 mb-2 d-flex"><span class="strong"> </span><input type="text" class="form-control" name="us[]"></div><p style=" margin-top: -29px;"> ${rowIdx}. </p></td><td><div class="mb-2 d-flex"><input type="text" class="form-control mr-2" name="uk[]"><a href="#" class="btn btn-sm btn-danger delete">&times;</a></div></td></tr>`); //add input box
     });
 
-    $(append).on('click', '.delete', function () {
-    var child = $(this).closest('tr').nextAll();
-        child.each(function () {
-            var id = $(this).attr('id');
-            var idx = $(this).children(append).children('p');
-            var dig = parseInt(id.substring(1));
-            idx.html(` ${dig - 1}.`);
-            $(this).attr('id', `R${dig - 1}`);
-        });
-    $(this).closest('tr').remove();
-    rowIdx--;
-    });
 
-    $(add_video).click(function(e) {
+      $(append).on('click', '.delete', function () {
+        var child = $(this).closest('tr').nextAll();
+            child.each(function () {
+                var id = $(this).attr('id');
+                var idx = $(this).children(append).children('p');
+                var dig = parseInt(id.substring(1));
+                idx.html(` ${dig - 1}.`);
+                $(this).attr('id', `R${dig - 1}`);
+            });
+        $(this).closest('tr').remove();
+        rowIdx--;
+      });
+
+      $(add_video).click(function(e) {
         e.preventDefault();
         if (y < max_fields) {
             y++;
@@ -59,7 +59,7 @@ $(document).ready(function() {
                     </td>
                     <td style="vertical-align: inherit;">
                         <div class="mb-2 d-flex">
-                            <a href="javascript:void(0)" class="btn btn-sm btn-danger deletes">&times;</a>
+                            <a href="#" class="btn btn-sm btn-danger deletes">&times;</a>
                         </div>
                     </td>
                 </tr>`); //add input box
@@ -81,6 +81,30 @@ $(document).ready(function() {
         $(this).closest('tr').remove();
         rowIdy--;
       });
+
+    $(document).on("click", ".delete", function(e) {
+        e.preventDefault();
+        $(this).closest('tr').remove();
+        x--;
+    })
+
+    $(document).on("click", ".deletes", function(e) {
+        e.preventDefault();
+        $.ajax({
+                type: "post",
+                url: $(".admin_url").attr("value"),
+                dataType: 'json',
+                data: {
+                    action: 'delete_data',
+                    post_id: $(".post_id").attr("value"), 
+                },
+                success: function(data) {
+                  console.log(data)
+                }
+            });
+        $(this).closest('tr').remove();
+        y--;
+    })
 
     $(function() {
         var $tabButtonItem = $('#tab-button li'),
@@ -115,6 +139,7 @@ $(document).ready(function() {
 
     function copyToClipboard(text) {
       if (window.clipboardData && window.clipboardData.setData) {
+        // IE specific code path to prevent textarea being shown while dialog is visible.
         return clipboardData.setData("Text", text);
 
       } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
