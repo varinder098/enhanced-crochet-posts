bf9c02efe361e5743b44abdbcb9fa93740a70908
harminderkaur9808 <balwinder.owninfotech@gmail.com>
2021-09-17 16:45:36 +0530

    Update metabox.php

--- include/metabox.php
+++ include/metabox.php
@@ -11,16 +11,12 @@ function custom_meta_box_markup($post_id) {
     $default_video = get_post_meta($id, "default_video", true);
     $left_handed_video = get_post_meta($id, "left_handed_video", true);
 
-
-   //echo"<pre>";print_r($left_handed_video);exit;
-
     $vcheck = array();
     if (empty($default_video)) {
         $vcheck = 0;
     } else {
         $vcheck = count($default_video);
     }
-    /**/
     $check = array();
     if (empty($uk)) {
         $check = 0;
@@ -60,7 +56,6 @@ function save_function_post_meta($post_id) {
     }
 
     $default_video = get_post_meta(get_the_ID(), "default_video", true);
-
     $left_handed_video = get_post_meta(get_the_ID(), "left_handed_video", true);//get existing data from db
 
     if(!empty($_POST['deleted_video'])) {
@@ -68,19 +63,13 @@ function save_function_post_meta($post_id) {
             array_splice($default_video, $values-$key, 1);
             array_splice($left_handed_video, $values-$key, 1);
          
-
-
-
-         
-        
             update_post_meta($post_id, 'default_video',$default_video);
             update_post_meta($post_id, 'left_handed_video',$left_handed_video);
+        }
     }
-}
-/*bbbb*/
 
     if(!empty($_FILES['default_video']['name'][0])) {
-       if($default_video)
+        if($default_video)
         {
             $url = $default_video;
             for($i = 0; $i < count($_FILES['default_video']['name']);$i++) 
@@ -98,12 +87,10 @@ function save_function_post_meta($post_id) {
                 $url[] = $upload['url'];
             }
         }
-
         update_post_meta($post_id, 'default_video',$url);
     }
     
-    if(!empty($_FILES['left_handed_video']['name'][0]))//if user upload some video
-    {
+    if(!empty($_FILES['left_handed_video']['name'][0])){
         if($left_handed_video)
         {
             $url=$left_handed_video;
@@ -113,7 +100,6 @@ function save_function_post_meta($post_id) {
                 $url[] = $upload['url'];
             }
         }
-        /**/
         else
         {
             $url=[];
@@ -125,9 +111,5 @@ function save_function_post_meta($post_id) {
         }
         update_post_meta($post_id, 'left_handed_video',$url);
     }
-
-
 }
 add_action('save_post', 'save_function_post_meta', 20, 3);
-
-/**/
