641fe105b7220042362aad75719c15c42af9cf68
harminderkaur9808 <harminderkaur9808@gmail.com>
2021-09-17 16:10:57 +0530

    Update admin.php

--- include/admin.php
+++ include/admin.php
@@ -187,17 +187,16 @@
 		</div>
 	</div>
 </div>
+
 <!-- The Modal -->
 <div class="modal" id="myModal">
   <div class="modal-dialog">
     <div class="modal-content">
-
       <!-- Modal Header -->
       <div class="modal-header">
         <h6 class="modal-title">Please update wordpress (upload limit) to proceed</h6>
         <button type="button" class="close" data-dismiss="modal">&times;</button>
       </div>
-
       <!-- Modal body -->
       <div class="modal-body">
       	<p class="text-success message"></p>
@@ -206,13 +205,11 @@
 		    <input type="text" class="form-control" placeholder="300mb" id="limit">
 		</div>
       </div>
-
       <!-- Modal footer -->
       <div class="modal-footer">
       	<button type="button" id="update_btn" class="btn btn-info">Update</button>
         <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
       </div>
-
     </div>
   </div>
 </div>
